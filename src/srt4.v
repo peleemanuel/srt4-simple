92828f1d45f3f98e4cf294950c54e2a278fb53dc
GeorgeT7 <george.pop@student.upt.ro>
2023-04-22 19:48:30 +0300

    Updated srt4.v and remade register.v cuz git

--- src/srt4.v
+++ src/srt4.v
@@ -5,4 +5,65 @@ module srt4 (
     output endSignal
 );
 
+wire [16:0]control_signals_wire;
+
+CU control_unit(
+    .beginSignal(),
+    .rst_b(),
+    .clk(),
+    .b7(),
+    .b(),
+    .msbp(),
+    .cnt1(),
+    .endSignal(),
+    .control_signals()
+);
+
+p P_register(
+    .c0(control_signals_wire[0]),
+    .c2(control_signals_wire[2]),
+    .c3(control_signals_wire[3]),
+    .c8(control_signals_wire[8]),
+    .c14(control_signals_wire[14]),
+    .inbit(),
+    .d(),
+    .outbit(),
+    .q()
+);
+
+a A_register(
+    .c0(control_signals_wire[0]),
+    .c2(control_signals_wire[2]),
+    .c3(control_signals_wire[3]),
+    .c4(control_signals_wire[4]),
+    .c7(control_signals_wire[7]),
+    .c13(control_signals_wire[13]),
+    .inbit(),
+    .d(),
+    .outbit(),
+    .q()
+);
+
+aprim Aprim_register(
+    .c0(control_signals_wire[0]),
+    .c3(control_signals_wire[3]),
+    .c5(control_signals_wire[5]),
+    .c6(control_signals_wire[6]),
+    .c12(control_signals_wire[12]),
+    .inbit(),
+    .d(),
+    .outbit(),
+    .q()
+);
+
+b B_register(
+    .c0(control_signals_wire[0]),
+    .inbit(),
+    .d(),
+    .outbit(),
+    .q()
+);
+
+
+
 endmodule
