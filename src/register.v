f2aef4bbd68cf70f142ff3e82533dd55ef11429f
GeorgeT7 <126348415+GeorgeT7@users.noreply.github.com>
2023-04-22 18:24:11 +0300

    Revert "Getting updates"

--- src/register.v
+++ src/register.v
@@ -1,135 +1,116 @@
+module rgst #(
+	parameter w=8
+)(
+	input clk, rst_b, ld,//clr, // in case we need clr signal
+	input [w-1:0] d,
+	output reg [w-1:0] q
+  //input c1 c2 c3 c4 etc
+  //input in_bit si ii tot dau pl in gura (am valoarea din main)
+  //output reg out_bit ii dau assign 
+);
+	always @ (posedge clk, negedge rst_b)
+	begin
+		if (!rst_b)			q <= 0;
+		//else if (clr)			q <= 0; // do we need a clr signal?
+		else if (ld)			q <= d;
+  end
+//posedge c2
+//wire in_bit din fiecare registru si fac legatura in main
+/*
+reg [w:0]aux;
+reg outbuss;
+
+  always @(posedge clk)
+  begin
+    if(!ld)
+      begin
+        aux = Shift(q,1,0,1);      
+        outbuss = aux[w];
+        q <= aux[w-1:0];
+      end
+  end
+*/
+
+/*
+always @(posedge clk)
+begin
+  if(!ld)
+    begin
+      q <= Shift_k(q,2);
+    end  
+end
+*/
+
+function [w:0] Shift(input [w-1:0]P,input in_bit, input left, input right); // this function is only used my P register
+  reg out_bit;
+  begin
+    
+    if(left)
+    begin
+      out_bit = P[w-1];
+      P[w-1:1] = P[w-2:0];
+      P[0] = in_bit;
+    end  
+  else if(right)
+    begin
+      out_bit = P[0];
+      P[w-2:0] = P[w-1:1];
+      P[w-1] = in_bit;
+    end
+  Shift = {out_bit,P};
+  end
+  
+endfunction
+
+function [w-1:0] Shift_k(input [w-1:0]P, input [2:0]k);
+  Shift_k = P >> k;
+endfunction
+
+endmodule
+
+// dim 9
 module p (
-  input c0,c2,c3,c8,c14,
+  input c0,c2,
   input [1:0]inbit,
-  input [8:0]d, // intrarea pentru registru 
-  output reg [1:0]outbit, // cand shiftez cu 2 pozitii, o sa imi iasa 2 biti
-  output reg [8:0]q //iesirea pentru registru
+  input d,
+  output outbit,
+  output reg q
 );
 
 
-always @(posedge c0 or posedge c2 or posedge c3 or posedge c8 or posedge c14) begin
-  if(c0)
-    q <= 0;
-
+always @(posedge c0, posedge c2, posedge c3, posedge c8) begin
+  if(c0) q <= 0;
   else if(c2) begin
     q[8:1] <= q[7:0];
     q[0] <= inbit[0];
   end 
-
   else if(c3) begin
-    q[8:2] <= q[6:0];
-    q[1:0] <= inbit;
+  begin
+      q[8:2] <= q[6:0];
+      q[1:0] <= inbit;
+    end
   end
-  
-  else if(c8) begin // nu mai trebuie tratat cazul de c9 sau c10
+  else if(c8) begin
    q <= d;
-  end
-
-  else if(c14) begin
-    q[7:0] <= q[8:1];
-    q[8] <= 0;
   end
 end
 
 endmodule
-
+//restul de 8
 module a (
-  input c0,c2,c3,c4,c7,c13,
-  input [1:0]inbit,
-  input [7:0]d,
-  output reg [1:0]outbit,
-  output reg [7:0]q
+  ports
 );
   
-always @(posedge c0 or posedge c2 or posedge c3 or posedge c4 or posedge c7 or posedge c13) begin
-  if(c0) begin
-    q <= d;
-  end
-
-  else if(c2) begin
-    outbit[0] <= q[7];
-    q[7:1] <= q[6:0];
-    q[0] <= inbit[0];
-  end
-
-  else if(c3) begin
-    outbit <= q[7:6];
-    q[7:2] <= q[5:0];
-
-    if(c4) begin
-      q[1:0] <= 2'b01;
-    end
-    else if(c7) begin
-      q[1:0] <= 2'b10;
-    end
-
-    else begin
-      q[1:0] <= 2'b00;
-    end
-  end
-
-  else if(c13) begin
-    q <= d;
-  end
-  
-end
-
 endmodule
 
 module aprim (
-  input c0, c3, c5, c6, c12,
-  input [1:0]inbit,
-  input [7:0]d,
-  output reg [1:0]outbit,
-  output reg [7:0]q
+  ports
 );
   
-always @(posedge c0 or posedge c3 or posedge c5 or posedge c6 or posedge c12) begin
-  if(c0) begin
-    q <= '0;
-  end
-
-  else if (c3) begin
-    q[7:2] <= q[5:0];
-
-    if(c5) begin
-      q[1:0] <= 2'b01;
-    end
-
-    else if (c6) begin
-      q[1:0] <= 2'b10;
-    end
-
-    else 
-      q[1:0] <= 2'b00;
-  end
-
-  else if (c12) //! trebuie verificat pentru ca nu stiu daca este c8 sau c12
-    q <= d;
-end
-
 endmodule
 
 module b (
-  input c0,
-  input [1:0]inbit,
-  input [7:0]d,
-  output reg [1:0]outbit, //! trebuie verificata logica de pe outbit din acest modul
-  output reg [7:0]q
-);
-
-always @(posedge c1 or posedge c2) begin
   
-  if(c1) begin
-    q <= d;
-  end
-
-  else if(c2) begin
-    outbit[1] <= q[7];
-    q[7:1] <= q[6:0];
-    q[0] <= 1'b0;
-  end
-
-end
-
+);
+  
 endmodule
